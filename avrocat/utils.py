bcde0bfd2dc1c67c0409c1a81391e83a8bfdf838
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-12-10 15:26:16 +0100

    Add utils.py

---
+++ avrocat/utils.py
@@ -0,0 +1,8 @@
+from typing import Union
+
+
+def format_extra_config(extra_config):
+    if extra_config:
+        extra_config = [a.split('=') for a in extra_config.split(',')]
+        extra_config = dict((k, v) for k, v in extra_config)
+    return extra_config
