229057bbde793cb9659d579be3620b6857def1dc
Simon Wahlgren <simon.wahlgren@fyndiq.se>
2018-12-20 14:18:37 +0100

    Setup logging before importing

--- avrocat/__init__.py
+++ avrocat/__init__.py
@@ -1,12 +1,13 @@
 import pkg_resources
 
-from avrocat.consumer import Consumer
-from avrocat.producer import Producer
 from avrocat import log_config
 
 __version__ = pkg_resources.require("avrocat")[0].version
 
 log_config.setup()
+
+from avrocat.consumer import Consumer  # isort: skip  # noqa
+from avrocat.producer import Producer  # isort: skip  # noqa
 
 
 class AvroCat:
