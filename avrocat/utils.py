db5043b4a9d6c2ad6a3d7248a8fb4ce159868138
Simon Wahlgren <simon.wahlgren@gmail.com>
2025-09-18 14:44:49 +0200

    feat(producer): add support for producing headers

--- avrocat/utils.py
+++ avrocat/utils.py
@@ -3,3 +3,15 @@ def format_extra_config(extra_config):
         extra_config = [a.split("=") for a in extra_config.split(",")]
         extra_config = dict((k, v) for k, v in extra_config)
     return extra_config
+
+
+def parse_headers(headers):
+    parsed_headers = {}
+    for header in headers:
+        if "=" not in header:
+            raise ValueError(
+                f"Invalid header format: '{header}'. Expected format: key=value"
+            )
+        key, value = header.split("=", 1)
+        parsed_headers[key] = value
+    return parsed_headers
